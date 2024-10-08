4252c1ee0e6c8c2c5438ba25a95057c0df227e62
Alexis <AlexDdevs@gmail.com>
2024-10-08 18:37:19 +0200

    ✨ Create a landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,30 @@
-import Header from "./components/Header";
-import NavBar from "./components/NavBar";
+"use client"
+import ShinyButton from "./components/UI/ShinyButton";
+import Particles from "./components/UI/Particules";
 
 export default function Home() {
     return (
-        <div className="bg-bgContainer shadow-[inset_0_0_0_1px_#1D1D1D] h-full">
-            <Header />
-            <main>
-                <NavBar />
+        <>
+            <main className="bg-bgHero flex flex-col items-center justify-center flex-1 text-center gap-y-8 relative overflow-hidden">
+                <h1 className="font-semibold text-7xl text-white flex flex-col leading-snug z-50">
+                    <p>
+                        Unlock the
+                        <span className="gradientText mx-4">Full Potential</span>
+                        of
+                    </p>
+                    <p>
+                        <span className="gradientText mx-4">Your Design</span>
+                        with Paste
+                    </p>
+                </h1>
+                <p className="text-white text-lg max-w-[430px]">Ready-to-use components for stunning designs. Export seamlessly to Webflow and Figma.</p>
+                <ShinyButton
+                    text="Get Started Now"
+                />
+                <div className="absolute top-1/2 left-1/3 bg-grayText w-[1500px] h-[150px] rounded-full blur-[60px] rotate-[-45deg]"></div>
+                <div className="absolute top-1/2 right-1/3 bg-grayText w-[1500px] h-[150px] rounded-full blur-[60px] rotate-[45deg]"></div>
+                <Particles />
             </main>
-        </div>
+        </>
     );
 }
