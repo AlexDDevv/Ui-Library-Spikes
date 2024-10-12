1070f588d383707e6c9682d8b765583f2050ba28
Alexis <AlexDdevs@gmail.com>
2024-10-12 15:37:19 +0200

    💄 Add a best offer for a plan and use ShinyButton

--- src/app/components/PrincingPlan.tsx
+++ src/app/components/PrincingPlan.tsx
@@ -1,18 +1,25 @@
 import React from 'react'
 import { Check } from "lucide-react"
+import ShinyButton from './UI/ShinyButton'
 
 type PrincingPlanProps = {
     plan: string,
+    best?: string,
     price: number,
     avantages: string[]
 }
 
-export default function PrincingPlan({ plan, price, avantages }: PrincingPlanProps) {
+export default function PrincingPlan({ plan, best, price, avantages }: PrincingPlanProps) {
     return (
-        <article className='bg-card rounded-xl shadow-forCard p-4 w-[300px] max-w-[350px] h-full'>
-            <div className='bg-bgHero px-6 py-5 shadow-forCard rounded-lg mb-5'>
-                <div className='flex items-center justify-between gap-x-5'>
-                    <h3 className='font-semibold text-grayText text-lg mb-8'>{plan}</h3>
+        <article className='bg-card rounded-xl shadow-forCard p-4 min-w-[325px] max-w-[350px] h-full'>
+            <div className='bg-bgHero px-6 py-5 shadow-[0_5px_15px_0_rgb(126,127,129,0.1)] rounded-lg mb-5'>
+                <div className='flex items-center justify-between gap-x-5 mb-8'>
+                    <h3 className='font-semibold text-grayText text-lg'>{plan}</h3>
+                    {best && (
+                        <ShinyButton
+                            text={best}
+                        />
+                    )}
                 </div>
                 <div className='flex items-center gap-x-3 mb-6'>
                     <h4 className='font-medium text-grayText text-4xl'>${price}</h4>
@@ -21,7 +28,7 @@ export default function PrincingPlan({ plan, price, avantages }: PrincingPlanPro
                 <button className='bg-btn text-white font-medium w-full rounded-md px-2 py-2 h-10 shadow-forCard'>Get started</button>
             </div>
             <div>
-                <ul className='pl-5'>
+                <ul className='pl-6'>
                     {avantages.map(avantage => (
                         <li key={avantage} className="flex items-center gap-x-2 text-white mb-3">
                             <Check />
