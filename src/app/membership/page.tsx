e4a2efecffbd8402f7c85a34d67e24445332699f
Alexis <AlexDdevs@gmail.com>
2024-10-19 08:29:52 +0200

    ✨ Use Marquee instead of Globe and change title

--- src/app/membership/page.tsx
+++ src/app/membership/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import NavBar from "../components/NavBar";
-import Globe from '../components/UI/Globe';
+import Marquee from '../components/UI/Marquee';
 import data from "../data/data.json"
 import PrincingPlan from '../components/PrincingPlan';
 
@@ -8,11 +8,11 @@ export default function page() {
     return (
         <main className='flex'>
             <NavBar />
-            <section className='bg-bgHero w-full p-8'>
+            <section className='bg-bgHero w-full p-8 overflow-hidden'>
                 <div className='flex gap-8 h-[388px] border-b border-border mb-8 pb-8'>
                     <div className='w-1/2'>
                         <h1 className='font-semibold gradientText text-4xl mb-5'>
-                            Join the Paste community
+                            Rise your level
                         </h1>
                         <div className='flex flex-col gap-y-3'>
                             <p className='text-sm text-grayText max-w-[500px] leading-relaxed'>Join our passionate developer community and unlock access to exclusive component libraries, powerful extensions, and dedicated support.</p>
@@ -20,8 +20,10 @@ export default function page() {
                             <p className='text-sm text-grayText max-w-[500px] leading-relaxed'> Whether you're a beginner or a pro, our platform helps you save time and build better, faster!</p>
                         </div>
                     </div>
-                    <div className="flex items-center justify-center w-1/2 bg-card rounded-xl h-full shadow-forCard overflow-hidden">
-                        <Globe />
+                    <div className="flex items-center justify-center w-1/2 bg-bgContainer rounded-xl h-full border-2 border-border overflow-x-hidden py-6 relative">
+                        <Marquee reverse />
+                        <div className="pointer-events-none absolute inset-y-0 left-0 w-5 bg-gradient-to-r from-bgContainer"></div>
+                        <div className="pointer-events-none absolute inset-y-0 right-0 w-5 bg-gradient-to-l from-bgContainer"></div>
                     </div>
                 </div>
                 <section>
