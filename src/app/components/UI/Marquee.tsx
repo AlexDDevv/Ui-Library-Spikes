732e406ecbd2ddc003aa825f92091649bbfb5ae7
Alexis <AlexDdevs@gmail.com>
2024-10-19 08:29:24 +0200

    ✨ Create Marquee component

---
+++ src/app/components/UI/Marquee.tsx
@@ -0,0 +1,40 @@
+import MarqueeCard from "./MarqueeCard";
+import data from "../../data/data.json"
+
+interface MarqueeProps {
+    children?: React.ReactNode;
+    repeat?: number;
+    [key: string]: any;
+}
+
+export default function Marquee({
+    children,
+    repeat = 4,
+    ...props
+}: MarqueeProps) {
+    return (
+        <div
+            {...props}
+            className="group flex overflow-hidden p-2 [--gap:1rem] [gap:var(--gap)] flex-row [--duration:40s]"
+        >
+            {Array(repeat)
+                .fill(0)
+                .map((_, i) => (
+                    <div
+                        key={i}
+                        className="flex shrink-0 justify-around [gap:var(--gap)] animate-marquee flex-row"
+                    >
+                        {data.marquee.map(item => (
+                            <MarqueeCard
+                                key={item.name}
+                                name={item.name}
+                                username={item.username}
+                                body={item.body}
+                                img={item.img}
+                            />
+                        ))}
+                    </div>
+                ))}
+        </div>
+    );
+}
