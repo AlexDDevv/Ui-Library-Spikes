bf8cfe8c412efbc2dd4f3148a152ef160bc12e3b
Alexis <AlexDdevs@gmail.com>
2024-10-19 08:30:26 +0200

    💄 Modif PricingPlan

--- src/app/components/PrincingPlan.tsx
+++ src/app/components/PrincingPlan.tsx
@@ -11,10 +11,10 @@ type PrincingPlanProps = {
 
 export default function PrincingPlan({ plan, best, price, avantages }: PrincingPlanProps) {
     return (
-        <article className='bg-card rounded-xl shadow-forCard p-4 min-w-[325px] max-w-[350px] h-full'>
-            <div className='bg-bgHero px-6 py-5 shadow-[0_5px_15px_0_rgb(126,127,129,0.1)] rounded-lg mb-5'>
+        <article className='bg-bgContainer rounded-xl shadow-forCard p-4 min-w-[325px] max-w-[350px] h-full'>
+            <div className='bg-transparent border border-border px-6 py-5 shadow-[inset_0_-10px_30px_-10px_#ffffff1f] rounded-lg mb-5'>
                 <div className='flex items-center justify-between gap-x-5 mb-8'>
-                    <h3 className='font-semibold text-grayText text-lg'>{plan}</h3>
+                    <h3 className='font-semibold text-white text-lg'>{plan}</h3>
                     {best && (
                         <ShinyButton
                             text={best}
@@ -22,15 +22,15 @@ export default function PrincingPlan({ plan, best, price, avantages }: PrincingP
                     )}
                 </div>
                 <div className='flex items-center gap-x-3 mb-6'>
-                    <h4 className='font-medium text-grayText text-4xl'>${price}</h4>
-                    <p className='text-grayText'>/ month</p>
+                    <h4 className='font-medium text-white text-4xl'>${price}</h4>
+                    <p className='text-white'>/ month</p>
                 </div>
                 <button className='bg-btn text-white font-medium w-full rounded-md px-2 py-2 h-10 shadow-forCard'>Get started</button>
             </div>
             <div>
                 <ul className='pl-6'>
                     {avantages.map(avantage => (
-                        <li key={avantage} className="flex items-center gap-x-2 text-white mb-3">
+                        <li key={avantage} className="flex items-center gap-x-2 text-grayText mb-3">
                             <Check />
                             {avantage}
                         </li>
