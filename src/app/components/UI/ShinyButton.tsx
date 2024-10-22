d801a62cb738abc8e729a27dd8e97f344ced0908
Alexis <AlexDdevs@gmail.com>
2024-10-22 21:39:59 +0200

    📱 Responsive for ShinyButtun

--- src/app/components/UI/ShinyButton.tsx
+++ src/app/components/UI/ShinyButton.tsx
@@ -42,11 +42,11 @@ const ShinyButton = ({ text }: shinyButtonProps) => {
     return (
         <motion.button
             {...animationProps}
-            className={`relative ${pathname === "/membership" ? "px-2 py-1 rounded-md" : "px-6 py-2.5 rounded-lg"} font-medium backdrop-blur-xl shadow-forCard bg-btn z-50`}
+            className={`relative ${pathname === "/membership" ? "px-2 py-1 rounded-md" : "px-6 py-2.5 rounded-lg max-sm:px-5 max-sm:py-2 max-sm:rounded-md"} font-medium backdrop-blur-xl shadow-forCard bg-btn z-50`}
             onClick={redirectToLibrary}
         >
             <span
-                className={`relative block h-full w-full ${pathname === "/membership" ? "text-xs normal-case text-grayText" : "text-sm uppercase text-white"} tracking-wide`}
+                className={`relative block h-full w-full ${pathname === "/membership" ? "text-xs normal-case text-grayText" : "text-sm uppercase text-white max-sm:text-xs"} tracking-wide`}
                 style={{
                     maskImage:
                         "linear-gradient(-75deg,#292929 calc(var(--x) + 20%),transparent calc(var(--x) + 30%),#1B1B1B calc(var(--x) + 100%))",
