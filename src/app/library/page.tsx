27e374c39543b7f3117e17e6277e5397aa235256
Alexis <AlexDdevs@gmail.com>
2024-10-27 14:35:29 +0100

    📱 auto height instead of fix height

--- src/app/library/page.tsx
+++ src/app/library/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         <main className="flex max-sm:flex-col">
             <NavBar />
             <div className="bg-bgHero w-full p-8 max-md:p-7">
-                <section className="flex gap-8 h-[388px] border-b border-border pb-8 mb-8 max-lg:h-80 max-md:pb-7 max-md:mb-7">
+                <section className="flex gap-8 h-[388px] border-b border-border pb-8 mb-8 max-lg:h-auto max-md:pb-7 max-md:mb-7">
                     <div className="w-1/2 h-full flex flex-col justify-between max-lg:w-full max-lg:gap-8">
                         <div>
                             <div className="flex items-center gap-x-4 mb-6">
