3ad84655710d64d16460f360da22583370462b89
Alexis <AlexDdevs@gmail.com>
2024-10-27 10:41:22 +0100

    📱 Use the new logo for responsive

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import { User, Search } from "lucide-react";
 import Image from "next/image";
 import logo from "../../assets/img/logo.svg";
+import logoMini from "../../assets/img/logoMini.png"
 import data from "../data/data.json";
 import Link from "next/link";
 import UserMenu from "./UI/UserMenu";
@@ -18,9 +19,24 @@ export default function Header() {
     };
 
     return (
-        <header className="bg-bgContainer flex items-center justify-between gap-x-5 px-8 py-6 w-full border-b-2 border-border relative max-sm:px-5 max-sm:py-4">
-            <Link href={"/"} className="cursor-pointer min-w-24 max-sm:min-w-14">
-                <Image src={logo} alt="Logo de la marque" />
+        <header className="bg-bgContainer flex items-center justify-between gap-x-5 px-8 py-6 w-full border-b-2 border-border relative max-sm:px-5 max-sm:py-4 max-sm:gap-0">
+            <Link href={"/"} className="cursor-pointer min-w-24 max-sm:min-w-6 max-sm:w-6">
+                <picture>
+                    <source
+                        media="(max-width: 640px)"
+                        srcSet={logoMini.src}
+                        width={24}
+                        height={24}
+                    />
+                    <Image
+                        src={logo}
+                        alt="Logo de la marque"
+                        className="max-sm:w-6 max-sm:h-6"
+                        width={96}
+                        height={33}
+                        priority
+                    />
+                </picture>
             </Link>
             <div className="w-[1px] h-6 bg-border max-lg:hidden"></div>
             <nav>
@@ -52,7 +68,7 @@ export default function Header() {
             </div>
             <div className="w-[1px] h-6 bg-border max-lg:hidden max-"></div>
             <div
-                className="bg-bgHero h-8 w-8 min-w-8 rounded-md shadow-forCard flex items-center justify-center cursor-pointer transition-shadow duration-200 ease-in-out hover:shadow-forCardHover z-[230] max-sm:h-6 max-sm:min-w-6 max-sm:w-6"
+                className="bg-bgHero h-8 w-8 min-w-8 rounded-md shadow-forCard flex items-center justify-center cursor-pointer transition-shadow duration-200 ease-in-out hover:shadow-forCardHover z-[230] max-sm:h-6 max-sm:min-w-6 max-sm:w-6 max-sm:rounded-[5px]"
                 onClick={handleShowMenu}
             >
                 <User className="text-white h-4 w-4 max-sm:h-3 max-sm:w-3" />
